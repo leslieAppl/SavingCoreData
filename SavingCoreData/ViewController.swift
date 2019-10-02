3aa52adaeb386a8c8dd5eac763c43fb94899a5f7
Leslie <leslie.appl@gmail.com>
2019-10-02 19:56:32 +0800

    Initial Commit

---
+++ SavingCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SavingCoreData
+//
+//  Created by leslie on 10/2/19.
+//  Copyright © 2019 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
